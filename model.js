b3595bb821829093eebf2e2eaea2f92a54f5e35f
Victoria Antonella <134626319+vickantonella@users.noreply.github.com>
2024-03-19 15:24:07 -0300

    Create model.js

---
+++ model.js
@@ -0,0 +1,10 @@
+// model.js
+
+// Dados de usuário (poderiam estar em um banco de dados)
+const usuarios = {
+    'usuario1': 'senha1',
+    'usuario2': 'senha2',
+    'victoria': '12345',
+};
+
+module.exports = { usuarios };
